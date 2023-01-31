d0feaca155a3040a303fe8801edbf97a3f40f490
bruno branda <brandabruno9@gmail.com>
2023-01-31 19:47:54 -0300

    second commit

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-
-
-function redirect (){
-document.getElementById("mk").addEventListener("click", function(){
+function redirect() {
+  document.getElementById("mk").addEventListener("click", function () {
     window.location.href = "https://mk11.mortalkombat.com/";
- });
- document.getElementById("sf").addEventListener("click", function(){
+  });
+  document.getElementById("sf").addEventListener("click", function () {
     window.location.href = "https://www.streetfighter.com/6/es-es/";
- });
- document.getElementById("dbz").addEventListener("click", function(){
-    window.location.href = "https://en.bandainamcoent.eu/dragon-ball/dragon-ball-fighterz";
- });
+  });
+  document.getElementById("dbz").addEventListener("click", function () {
+    window.location.href =
+      "https://en.bandainamcoent.eu/dragon-ball/dragon-ball-fighterz";
+  });
+  document.getElementById("dnf duel").addEventListener("click", function () {
+    window.location.href = "https://dd.nexon.com/en/main";
+  });
 }
 
 redirect();
 
-
 const audio = document.getElementById("my-audio");
 const playButton = document.getElementById("play-button");
 
-playButton.addEventListener("click", function() {
+playButton.addEventListener("click", function () {
   audio.play();
 });
-
-
