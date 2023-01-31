724ab0b18a1985ef51c0eb1d56263d8055791659
Nicolas Blanco <nblancoestefan@users.noreply.github.com>
2023-01-31 10:38:54 -0300

    added redirect function in js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function redirect (){
+document.getElementById("mk").addEventListener("click", function(){
+    window.location.href = "https://mk11.mortalkombat.com/";
+ });
+ document.getElementById("sf").addEventListener("click", function(){
+    window.location.href = "https://www.streetfighter.com/6/es-es/";
+ });
+ document.getElementById("dbz").addEventListener("click", function(){
+    window.location.href = "https://en.bandainamcoent.eu/dragon-ball/dragon-ball-fighterz";
+ });
+}
+
+redirect();
